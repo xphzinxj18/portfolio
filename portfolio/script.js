f4d6048d8fd53b2513af5b87815443633d8be892
xphzinxj18 <paulohenriquesilvaaraujo18@gmail.com>
2026-04-10 10:46:04 -0300

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,9 @@
+const links = document.querySelectorAll('nav a');
+
+links.forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute('href'))
+      .scrollIntoView({ behavior: 'smooth' });
+  });
+});
